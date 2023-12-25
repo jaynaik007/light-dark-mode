702c332aad9c1710c47b022370eddf7ccd589f2a
Jayendra Naik <jayendranaik007@gmail.com>
2023-12-26 00:58:54 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+
+// Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 /50%)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+}
+
+// Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255 255 255 /50%)";
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+}
+
+// Switch Theme Dynamically
+const switchTheme = (event) => {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    darkMode();
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    lightMode();
+  }
+};
+// Event listener
+toggleSwitch.addEventListener("change", switchTheme);
+
+// Check Local Storage for Them
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
